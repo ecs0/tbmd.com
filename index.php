a3fdfa9b474baacea095024d93e030d48c87e338
Bryan Bergen <bryanbergen@gmail.com>
2015-11-05 12:14:00 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to imdb.com</title>
+    <link rel="stylesheet" href="style/main.css">
+</head>
+<body>
+    <h1>Welcome to Bryan & Tim's Movie Database</h1>
+</body>
+</html>
