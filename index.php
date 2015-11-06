91583b0e65686594db17467597d305099398c3e6
Bryan Bergen <bryanbergen@gmail.com>
2015-11-05 16:10:04 -0800

    added test pages

--- index.php
+++ index.php
@@ -10,5 +10,11 @@
 </head>
 <body>
     <h1>Welcome to Bryan & Tim's Movie Database</h1>
+    <p>
+        <a href="html/tim.html">Tim's Page</a>
+    </p>
+    <p>
+        <a href="html/bryan.html">Bryan's Page</a>
+    </p>
 </body>
 </html>
