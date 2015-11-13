6bbfe7575a96ccded1967c1a00400fa088f13090
Bryan Bergen <bryanbergen@gmail.com>
2015-11-12 16:06:07 -0800

    #17 docs and server side image validation

--- php/Uploader.php
+++ php/Uploader.php
@@ -1,22 +1,36 @@
 <?php
 
+define('BASE_DIR', $_SERVER['DOCUMENT_ROOT']."/images/uploads/");
+
 /**
  * Created by PhpStorm.
  * User: tsayler
  * Date: 11/11/15
  * Time: 8:36 AM
+ * 
+ * This class will manage file uploads and server side image validation
+ * For each image uploaded, it will attempt to generate a unique id to avoid
+ * file name collisions.
  */
-
-define('BASE_DIR', $_SERVER['DOCUMENT_ROOT']."/images/uploads/");
-
 class Uploader {
 
     private $file;
     
+    /**
+     * Instantiate an Uploader with a single element from $_FILES
+     * 
+     * @param type $file
+     */
     public function __construct($file) {
         $this->file = $file;
     }
 
+    /**
+     * Uploads the file contents to the webserver and returns the unique
+     * file name, including extension
+     * 
+     * @return boolean|string
+     */
     public function upload() {
         
         // create unique file name
@@ -24,12 +38,20 @@ class Uploader {
         $extension = pathinfo($name, PATHINFO_EXTENSION);
         $hashed_name =  md5($name + uniqid(time())).".".$extension;
         
+        // check if file actually an image
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_file($finfo, $this->file['tmp_name']);
+        if (!strstr($mime, "image")) {
+            return FALSE;
+        }
+        finfo_close($finfo);
+        
         // make dir if needed
         if (!is_dir(BASE_DIR)) {
             mkdir(BASE_DIR);
         }
         
-        // upload
+        // finally upload
         if (is_uploaded_file($this->file['tmp_name']) && 
                 move_uploaded_file($this->file['tmp_name'], BASE_DIR.$hashed_name)) {
             return $hashed_name;
