49af4021bf003feb56293559e591737097aa7f81
Bryan Bergen <bryanbergen@gmail.com>
2015-11-13 20:09:48 -0800

    #19: add php linting to build script

--- meta/build.sh
+++ meta/build.sh
@@ -3,3 +3,16 @@
 echo "Building Database"
 mysql --user="root" --password="" < ./database/schema.sql
 
+function lint {
+    for file in $@
+    do
+        if [[ $file == *.php ]]
+        then
+            echo "Linting $file..."
+            php -l $file
+        fi
+    done
+}
+
+lint ./php/*
+lint *
